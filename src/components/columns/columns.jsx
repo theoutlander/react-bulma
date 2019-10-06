ce624d83b11032cd0afd79c9e739e3d581d5f862
Nick Karnik <theoutlander@live.com>
2019-10-05 18:30:04 -0700

    Fixes

--- src/components/columns/columns.jsx
+++ src/components/columns/columns.jsx
@@ -1,28 +1,27 @@
-import "bulma";
-import React from "react";
-import { mapClasses } from "../util";
-import PropTypes from "prop-types";
+import 'bulma';
+import React from 'react';
+import { mapClasses } from '../util';
+import PropTypes from 'prop-types';
 
-const Column = props => {
-  let classes = mapClasses(props);
-  if (props.gap >= 0) {
-    classes = `is-variable ${classes}`;
-  }
-  // console.log(classes);
-  return <div className={`columns ${classes}`}>{props.children}</div>;
+const Columns = (props) => {
+	let classes = mapClasses(props);
+	if (props.gap >= 0) {
+		classes = `is-variable ${classes}`;
+	}
+	return <div className={`columns ${classes}`}>{props.children}</div>;
 };
 
-Column.propTypes = {
-  gap: PropTypes.number,
-  multiline: PropTypes.bool,
-  desktopGap: PropTypes.number,
-  mobileGap: PropTypes.number,
-  tabletGap: PropTypes.number,
-  touchGap: PropTypes.number,
-  widescreenGap: PropTypes.number,
-  fullhdGap: PropTypes.number,
-  vCentered: PropTypes.bool,
-  centered: PropTypes.bool
+Columns.propTypes = {
+	gap: PropTypes.number,
+	multiline: PropTypes.bool,
+	desktopGap: PropTypes.number,
+	mobileGap: PropTypes.number,
+	tabletGap: PropTypes.number,
+	touchGap: PropTypes.number,
+	widescreenGap: PropTypes.number,
+	fullhdGap: PropTypes.number,
+	vCentered: PropTypes.bool,
+	centered: PropTypes.bool
 };
 
-export default Column;
+export default Columns;
